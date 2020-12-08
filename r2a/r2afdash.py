0fd703023a06db55b12e076ab8131dd981e7741b
AlvaroLuz <alvaroluz2001@gmail.com>
2020-12-08 19:10:26 -0200

    Algumas mudança para a funcionalidade do código

--- r2a/r2afdash.py
+++ r2a/r2afdash.py
@@ -23,16 +23,17 @@ class R2AFDash(IR2A):
         IR2A.__init__(self, id)
         self.parsed_mpd = ''
         self.qi = []
-        self.qi_index = 9
+        self.qi_index = 0
         self.time_last_buffer_check = 0
         self.now_diff_buffer_time = 0
         self.first_package = True
         self.buffer_time = 0
         self.timestamp_last_buffer_add =0
         self.before_diff_buffer_time = -1
-        self.request_time = 0
+        self.handle_xml_request_time = 0
         self.ri = []
-
+        self.request_time = 0
+        self.throughputs = []
     def handle_xml_request(self, msg):
         # getting the initial time of the request for calculating the throughput
         self.request_time  = time.perf_counter()
@@ -52,26 +53,27 @@ class R2AFDash(IR2A):
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-
+        self.request_time  = time.perf_counter()
         # time to define the segment quality choose to make the request
         #variable to the first passage
         #can be improved
         if(self.first_package == False):
             buffer_playback = self.whiteboard.get_playback_buffer_size()
             i = 0   
-            for playback in buffer_playback:
-                if (playback[0] > self.timestamp_last_buffer_add):
-                    if ((playback[1]> buffer_playback[i-1][1])or(len(buffer_playback) == 1)):
-                        self.now_diff_buffer_time = playback[1] - self.time_last_buffer_check
-                        self.time_last_buffer_check = playback[1] - 1
-                        self.timestamp_last_buffer_add = playback[0]
+            for register in buffer_playback:
+                if (register[0]<self.timestamp_last_buffer_add):
+                    if((register[1]>buffer_playback[i-1][1])or(len(buffer_playback) == 1)):
+                        self.time_last_buffer_check = register[1] - 1
+                if( i == (len(buffer_playback)-1)):
+                    self.now_diff_buffer_time = register[1] - self.time_last_buffer_check
+                    self.timestamp_last_buffer_add = register[0]
                 i = i + 1
-            
+
             #verbal variables for referring to the buffer size
             Close, Long, Short = False, False, False
-            if(self.time_last_buffer_check<10):
+            if(self.time_last_buffer_check<35):
                 Short = True 
-            elif(self.time_last_buffer_check>=20):
+            elif(self.time_last_buffer_check>=50):
                 Long = True
             else:
                 Close = True
@@ -87,20 +89,9 @@ class R2AFDash(IR2A):
             else:
                 Steady = True
             
-            #choosing the non null minimum value
-            if ((delta_diff_buffer_time<self.time_last_buffer_check)):
-                if(delta_diff_buffer_time == 0):
-                    r = self.time_last_buffer_check
-                else:
-                    r = delta_diff_buffer_time
-            else:
-                if (self.time_last_buffer_check == 0):
-                    r = delta_diff_buffer_time
-                else:
-                    r = self.time_last_buffer_check
-            
             #evaluating the controllers and rules
             r1,r2,r3,r4,r5,r6,r7,r8,r9 = 0,0,0,0,0,0,0,0,0
+            r = 1
             print(f'Controladores: {self.time_last_buffer_check},{delta_diff_buffer_time}')
             if Short   and Falling:
                 r1 = r
@@ -167,14 +158,12 @@ class R2AFDash(IR2A):
 
     def handle_segment_size_response(self, msg):
         #getting the bit size of the message to calculate throughput
-        #is it necessary to be here
-        buffer_playback = self.whiteboard.get_playback_buffer_size()
-        i = 0
-        for playback in buffer_playback:
-            if (playback[0] > self.timestamp_last_buffer_add):
-                if ((playback[1]> buffer_playback[i-1][1])or(len(buffer_playback) == 1)):
-                    self.before_diff_buffer_time = self.now_diff_buffer_time
-            i = i + 1
+        request_processing_time = time.perf_counter() - self.request_time
+        bit_size = msg.get_bit_length()
+
+        self.ri.append(bit_size/(request_processing_time*2))
+
+        self.before_diff_buffer_time = self.now_diff_buffer_time
 
         self.send_up(msg)
 
